8a1a26dcb0cbf27e09ea2cc839df407f6e334f8d
Matthew Prestifilippo <matthew.prestifilippo@syapse.com>
2019-07-19 11:42:03 -0400

    Add flag to get response object

--- src/fetch-suspense.ts
+++ src/fetch-suspense.ts
@@ -1,8 +1,6 @@
-import deepEqual = require('deep-equal');
+import deepEqual = require("deep-equal");
 
-
-
-type CreateUseFetch = (fetch: GlobalFetch['fetch']) => UseFetch;
+type CreateUseFetch = (fetch: GlobalFetch["fetch"]) => UseFetch;
 
 interface Export extends UseFetch {
   createUseFetch: CreateUseFetch;
@@ -17,44 +15,40 @@ interface FetchCache {
   response?: any;
 }
 
+interface UseFetchOptions {
+  lifespan?: number;
+  info?: boolean;
+}
+
 type UseFetch = (
   input: RequestInfo,
   init?: RequestInit | undefined,
-  lifespan?: number,
+  options?: UseFetchOptions | number
 ) => Object | string;
 
-
-
 const createUseFetch: CreateUseFetch = (
-  fetch: GlobalFetch['fetch'],
+  fetch: GlobalFetch["fetch"]
 ): UseFetch => {
-
   // Create a set of caches for this hook.
   const caches: FetchCache[] = [];
 
   return (
     input: RequestInfo,
     init?: RequestInit | undefined,
-    lifespan: number = 0,
+    options?: UseFetchOptions | number
   ): Object | string => {
-
     // Check each cache by this useFetch hook.
     for (const cache of caches) {
-
       // If this cache matches the request,
-      if (
-        deepEqual(input, cache.input) &&
-        deepEqual(init, cache.init)
-      ) {
-
+      if (deepEqual(input, cache.input) && deepEqual(init, cache.init)) {
         // If an error occurred, throw it so that componentDidCatch can handle
         //   it.
-        if (Object.prototype.hasOwnProperty.call(cache, 'error')) {
+        if (Object.prototype.hasOwnProperty.call(cache, "error")) {
           throw cache.error;
         }
-  
+
         // If a response was successful, return it.
-        if (Object.prototype.hasOwnProperty.call(cache, 'response')) {
+        if (Object.prototype.hasOwnProperty.call(cache, "response")) {
           return cache.response;
         }
 
@@ -66,57 +60,69 @@ const createUseFetch: CreateUseFetch = (
 
     // If no request in the cache matched this one, create a new cache entry.
     const cache: FetchCache = {
-
       // Make the fetch request.
       fetch: fetch(input, init)
-
         // Parse the response.
-        .then((response: Response): Promise<Object | string> => {
-          const contentType: null | string =
-            response.headers.get('Content-Type');
-          if (
-            contentType &&
-            contentType.indexOf('application/json') !== -1
-          ) {
-            return response.json();
+        .then(
+          (response: Response): Promise<Object | string> => {
+            options = options || {};
+            let ret;
+            const contentType: null | string = response.headers.get(
+              "Content-Type"
+            );
+            if (contentType && contentType.indexOf("application/json") !== -1) {
+              ret = response.json();
+            }
+            ret = response.text();
+            if (typeof options === 'object' && options.info) {
+              return ret.then((data) => ({ data, response }));
+            }
+            return ret;
           }
-          return response.text();
-        })
+        )
 
         // Cache the response.
-        .then((response: Object | string): void => {
-          cache.response = response;
-        })
+        .then(
+          (response: Object | string): void => {
+            cache.response = response;
+          }
+        )
 
         // Handle an error.
-        .catch((e: Error): void => {
-          cache.error = e;
-        })
+        .catch(
+          (e: Error): void => {
+            cache.error = e;
+          }
+        )
 
         // Invalidate the cache.
-        .then((): void => {
-          if (lifespan > 0) {
-            setTimeout(
-              (): void => {
+        .then(
+          (): void => {
+            let lifespan = 0;
+            if (typeof options === 'number') {
+              lifespan = options;
+            } else if (typeof options === 'object') {
+              lifespan = options.lifespan || 0;
+            }
+            if (lifespan > 0) {
+              setTimeout((): void => {
                 const index: number = caches.indexOf(cache);
                 if (index !== -1) {
                   caches.splice(index, 1);
                 }
-              },
-              lifespan,
-            );
+              }, lifespan);
+            }
           }
-        }),
+        ),
       init,
-      input,
+      input
     };
     caches.push(cache);
     throw cache.fetch;
   };
 };
 
-const _export: Export = Object.assign(
-  createUseFetch(window.fetch), {
+const _export: Export = Object.assign(createUseFetch(window.fetch), {
   createUseFetch,
   default: createUseFetch(window.fetch)
 });
